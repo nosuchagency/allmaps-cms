83f5aaf27097823fdca171d12b8c8d43e39c00c6
Victor-Emil <victoremil.r.andersen@gmail.com>
2019-06-06 11:31:45 +0200

    changed google mutant to mapbox tile

--- resources/js/views/beacons/simple-map.vue
+++ resources/js/views/beacons/simple-map.vue
@@ -42,11 +42,18 @@
                     .addTo(this.map)
                     .bindPopup('Latitude: ' + this.latitude + '<br>' + 'Longitude: ' + this.longitude);
 
-                L.gridLayer
-                    .googleMutant({
-                        type: 'roadmap'
-                    })
-                    .addTo(this.map);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmljdG9yLWVtaWwiLCJhIjoiY2p2bnEwNGF5MWt4cjQ4bno5M2lrbnR5MiJ9.lI1yQnR0jvfZo-oLwfLKWQ', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'mapbox.streets',
+                    accessToken: 'pk.eyJ1IjoidmljdG9yLWVtaWwiLCJhIjoiY2p2bnEwNGF5MWt4cjQ4bno5M2lrbnR5MiJ9.lI1yQnR0jvfZo-oLwfLKWQ'
+                }).addTo(this.map);
+
+                // L.gridLayer
+                //     .googleMutant({
+                //         type: 'roadmap'
+                //     })
+                //     .addTo(this.map);
 
                 this.map.zoomControl.setPosition('bottomleft');
 
--- resources/js/views/places/map-location-select.vue
+++ resources/js/views/places/map-location-select.vue
@@ -56,9 +56,16 @@
                     this.setPopupContent(this.markerPopupContent);
                 }
 
-                L.gridLayer
-                    .googleMutant({type: 'roadmap'})
-                    .addTo(this.map);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmljdG9yLWVtaWwiLCJhIjoiY2p2bnEwNGF5MWt4cjQ4bno5M2lrbnR5MiJ9.lI1yQnR0jvfZo-oLwfLKWQ', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'mapbox.streets',
+                    accessToken: 'pk.eyJ1IjoidmljdG9yLWVtaWwiLCJhIjoiY2p2bnEwNGF5MWt4cjQ4bno5M2lrbnR5MiJ9.lI1yQnR0jvfZo-oLwfLKWQ'
+                }).addTo(this.map);
+
+                // L.gridLayer
+                //     .googleMutant({type: 'roadmap'})
+                //     .addTo(this.map);
 
                 this.map.zoomControl.setPosition('bottomleft');
 
